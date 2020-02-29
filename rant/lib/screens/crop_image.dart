66aad8046190d8dbf60e3836e834ef733aa0f644
Usama3074 <usama.3074@hotmail.com>
2020-03-01 01:41:00 +0500

    Crop Work
    Working on Crop

--- rant/lib/screens/crop_image.dart
+++ rant/lib/screens/crop_image.dart
@@ -16,40 +16,107 @@ class _ImageCropState extends State<ImageCrop> {
       appBar: AppBar(
         title: Text('Crop Image'),
       ),
-      body: CustomPaint(
-        foregroundPainter: CropPainter(),
-        child: GestureDetector(
-          onScaleStart: (ScaleStartDetails details) {
-            print(details);
-            _previousScale = _scale;
-            setState(() {});
-          },
-          onScaleUpdate: (ScaleUpdateDetails details) {
-            print(details);
-            _scale = _previousScale * details.scale;
-            setState(() {});
-          },
-          onScaleEnd: (ScaleEndDetails details) {
-            print(details);
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: CustomPaint(
+              foregroundPainter: CropPainter(),
+              child: GestureDetector(
+                onScaleStart: (ScaleStartDetails details) {
+                  print(details);
+                  _previousScale = _scale;
+                  setState(() {});
+                },
+                onScaleUpdate: (ScaleUpdateDetails details) {
+                  print(details);
+                  _scale = _previousScale * details.scale;
+                  setState(() {});
+                },
+                onScaleEnd: (ScaleEndDetails details) {
+                  print(details);
 
-            _previousScale = _scale;
-            setState(() {});
-          },
-          child: RotatedBox(
-            quarterTurns: 0,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Transform(
-                  alignment: FractionalOffset.center,
-                  transform: Matrix4.diagonal3(Vector3(_scale, _scale, _scale)),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('assets/images/skiing.jpg'))),
-                  )),
+                  _previousScale = _scale;
+                  setState(() {});
+                },
+                child: RotatedBox(
+                  quarterTurns: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Transform(
+                        alignment: FractionalOffset.center,
+                        transform:
+                            Matrix4.diagonal3(Vector3(_scale, _scale, _scale)),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image:
+                                      AssetImage('assets/images/skiing.jpg'))),
+                        )),
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
+          Container(
+            height: 120,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            color: Colors.grey[200],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.image, size: 30),
+                        Text('Change Image'),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.refresh, size: 30),
+                        Text('Rotate 90'),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.rounded_corner, size: 30),
+                        Text('Reset  '),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.zoom_in, size: 30),
+                        Text('Zoom in'),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.zoom_out, size: 30),
+                        Text('Zoom out'),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 40, right: 40),
+                  height: 30,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onPressed: null,
+                    child: Text('Save'),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
 
       /*  Container(
@@ -81,7 +148,8 @@ class CropPainter extends CustomPainter {
 
     final path2 = Path();
     path2.addOval(Rect.fromCircle(
-        center: Offset(size.width / 2, size.height / 2), radius: 80));
+        center: Offset(size.width / 2, size.height / 2), radius: 110));
+
     final operation = PathOperation.difference;
 
     final path = Path.combine(operation, path1, path2);
@@ -89,12 +157,24 @@ class CropPainter extends CustomPainter {
     final paint = Paint()
       ..color = Colors.white.withOpacity(.5)
       ..style = PaintingStyle.fill;
+    final squarePaint = Paint()
+      ..color = Color(0xFF0099FF)
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 2;
 
     canvas.drawPath(path, paint);
+    canvas.drawLine(Offset(size.width / 4.6, size.height / 3.2),
+        Offset(size.width / 1.3, size.height / 3.2), squarePaint);
+    canvas.drawLine(Offset(size.width / 1.3, size.height / 3.2),
+        Offset(size.width / 1.3, size.height / 1.45), squarePaint);
+    canvas.drawLine(Offset(size.width / 4.6, size.height / 1.45),
+        Offset(size.width / 1.3, size.height / 1.45), squarePaint);
+    canvas.drawLine(Offset(size.width / 4.6, size.height / 1.45),
+        Offset(size.width / 4.6, size.height / 3.2), squarePaint);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
+    return false;
   }
 }
